cfcbef224c9a45c26519e46322cbc00bffb9a51c
Maria-Olaya <124837260+Maria-Olaya@users.noreply.github.com>
2025-11-09 18:30:04 -0500

    Add third mock news article to NoticiasMockProvider

--- src/main/java/com/proyecto/cabapro/service/NoticiasMockProvider.java
+++ src/main/java/com/proyecto/cabapro/service/NoticiasMockProvider.java
@@ -15,6 +15,7 @@ public class NoticiasMockProvider implements NoticiasProvider {
         List<NewsArticle> mock = new ArrayList<>();
         mock.add(new NewsArticle("Noticia de prueba 1", "Descripción 1", "http://link1.com", "", "MockSource"));
         mock.add(new NewsArticle("Noticia de prueba 2", "Descripción 2", "http://link2.com", "", "MockSource"));
+         mock.add(new NewsArticle("Noticia de prueba 3", "Descripción 3", "http://link3.com", "", "MockSource"));
         return mock;
     }
 }
